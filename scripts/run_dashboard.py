e7c1e1c2ec244f2229446ee7f227a686ebc0a28f
Léonard Pasi <leonardpasi@gmail.com>
2025-04-19 12:02:51 +0200

    updated connection string

--- scripts/run_dashboard.py
+++ scripts/run_dashboard.py
@@ -3,5 +3,5 @@ import os
 from trulens.core import TruSession
 from trulens.dashboard import run_dashboard
 
-tru = TruSession(database_url=os.getenv("SUPABASE_CONNECTION_STRING"))
+tru = TruSession(database_url=os.getenv("SUPABASE_PROD_CONNECTION_STRING_IPV4"))
 run_dashboard(tru)
